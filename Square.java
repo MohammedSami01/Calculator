335a5d816be3bdfec4ccc8db085af1cfd83fe87b
MohammedSami01 <156496772+MohammedSami01@users.noreply.github.com>
2024-02-19 15:03:07 +0530

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,23 +1,28 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Square {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // Input number from the user
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-        
-        // Calculate square
-        int square = number * number;
-        
-        // Calculate cube
-        int cube = number * number * number;
-        
-        // Display results
-        System.out.println("Square of " + number + " is: " + square);
-        System.out.println("Cube of " + number + " is: " + cube);
-        
-        scanner.close();
+        try {
+            // Input number from the user
+            System.out.print("Enter a number: ");
+            int number = scanner.nextInt();
+            
+            // Calculate square
+            int square = number * number;
+            
+            // Calculate cube
+            int cube = number * number * number;
+            
+            // Display results
+            System.out.println("Square of " + number + " is: " + square);
+            System.out.println("Cube of " + number + " is: " + cube);
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Please enter a valid integer.");
+        } finally {
+            scanner.close(); // Close the Scanner object to release system resources
+        }
     }
 }
