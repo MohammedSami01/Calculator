71b129bfbbea1b931feb87a9a4d222ac406a9554
Sami <01fe22bca063@kletech.ac.in>
2024-02-19 16:40:56 +0530

    updated Code for Square and Cube operations

---
+++ ext_calculator.java
@@ -0,0 +1,8 @@
+public class ext_calculator {
+    static float square(float operand_1){
+    return(operand_1 * operand_1);
+    }
+    static float cube(float operand_1){
+        return(operand_1*operand_1*operand_1);
+    }
+}
